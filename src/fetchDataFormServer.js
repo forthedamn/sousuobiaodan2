250bbbb9d539e9c27d901db214ecb8bb27a2b6e5
forthedamn <simonzhong0924@gmail.com>
2021-09-13 16:13:48 +0800

    2 files modified

--- src/fetchDataFormServer.js
+++ src/fetchDataFormServer.js
@@ -1,56 +1,56 @@
 const DataSource = [
   {
-    name: "A",
-    sex: "male",
+    name: 'A',
+    gender: 'male',
     fe: 0
   },
   {
-    name: "B",
-    sex: "female",
+    name: 'B',
+    gender: 'female',
     fe: 0
   },
   {
-    name: "C",
-    sex: "male",
+    name: 'C',
+    gender: 'male',
     fe: 1
   },
   {
-    name: "D",
-    sex: "male",
+    name: 'D',
+    gender: 'male',
     fe: 1
   },
   {
-    name: "E",
-    sex: "female",
+    name: 'E',
+    gender: 'female',
     fe: 1
   },
   {
-    name: "F",
-    sex: "male",
+    name: 'F',
+    gender: 'male',
     fe: 0
   },
   {
-    name: "G",
-    sex: "male",
+    name: 'G',
+    gender: 'male',
     fe: 0
   },
   {
-    name: "H",
-    sex: "female",
+    name: 'H',
+    gender: 'female',
     fe: 1
   }
 ];
-const getDataSource = async (query) => {
-  return new Promise((resolve) => {
+const getDataSource = async query => {
+  return new Promise(resolve => {
     if (!query) resolve(DataSource);
     resolve(
-      DataSource.filter((data) => {
+      DataSource.filter(data => {
         let validate = true;
         if (query.name) {
           validate = data.name === query.name;
         }
-        if (query.sex) {
-          validate = data.sex === query.sex;
+        if (query.gender) {
+          validate = data.gender === query.gender;
         }
         if (query.fe) {
           validate = data.fe === query.fe;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import "antd/dist/antd.css";
-import "./index.css";
-import { Select, Radio, Form, Row, Col, Input, Button, Table } from "antd";
-import fetchDataFormServer from "./fetchDataFormServer";
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Select, Radio, Form, Row, Col, Input, Button, Table } from 'antd';
+import fetchDataFormServer from './fetchDataFormServer';
 
 const SearchForm = () => {
   const [form] = Form.useForm();
@@ -11,7 +11,7 @@ const SearchForm = () => {
   const getDatasource = async (query = {}) => {
     // 模拟从服务端获取数据
     // 这里已经实现好了，你只需要完成下面这行之后的逻辑
-    const result = await fetchDataFormServer(query);
+    return await fetchDataFormServer(query);
   };
 
   return (
@@ -20,14 +20,14 @@ const SearchForm = () => {
         form={form}
         name="advanced_search"
         className="ant-advanced-search-form"
-      ></Form>
+      />
     </>
   );
 };
 
 ReactDOM.render(
-  <div style={{ paddingLeft: "20px" }}>
+  <div style={{ paddingLeft: '20px' }}>
     <SearchForm />
   </div>,
-  document.getElementById("container")
+  document.getElementById('container')
 );
